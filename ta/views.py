b8223db3095d3ee276576ccdbbccfd34cc34d631
vugarmammadli <v-m13@windowslive.com>
2019-08-04 17:36:26 -0400

    GET POST /applicants/, GET /applicants?status=status, GET /applicants?fname=fname implemented

--- ta/views.py
+++ ta/views.py
@@ -4,13 +4,32 @@ from rest_framework import generics
 from .serializers import TeachingAssistantSerializer
 from .models import TeachingAssistant
 
-# Create your views here.
-
 class TACreateView(generics.ListCreateAPIView):
     '''Handles the GET and POST requests of the rest api for TAs.
     '''
-    queryset = TeachingAssistant.objects.all()
     serializer_class = TeachingAssistantSerializer
+    
+    def get_queryset(self):
+        '''Return a list of all the applicants. Optionally filters applicants
+        according to status or family name.
+        '''
+        
+        queryset = TeachingAssistant.objects.all()
+        
+        # /applicants?status=status
+        status = self.request.query_params.get('status')
+        
+        # /applicants?fname=fname
+        fname = self.request.query_params.get('fname')
+        
+        # iexact used for filtering case insensitive
+        if status is not None:
+            queryset = queryset.filter(status__iexact=status)
+            
+        if fname is not None:
+            queryset = queryset.filter(family_name__iexact=fname)
+        
+        return queryset
     
     def perform_create(self, serializer):
         '''Save the post data when creating a new TA.
