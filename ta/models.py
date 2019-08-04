cb18f8ef6d930eb7a895219a6dac080adbce6add
vugarmammadli <v-m13@windowslive.com>
2019-08-04 15:49:17 -0400

    created basic models for ta application system

--- ta/models.py
+++ ta/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class TeachingAssistant(models.Model):
+    student_number = models.IntegerField(blank=False, unique=True)
+    given_name = models.CharField(max_length=255)
+    family_name = models.CharField(max_length=255)
+    status = models.CharField(max_length=5)
+    year = models.IntegerField(default=1)
+    
+    def __str__(self):
+        return "Given Name: {}\n Family Name: {}\n Student Number: {}\n \
+        Status: {}\n Year: {}".format(self.given_name, self.family_name,
+                                      self.student_number, self. status,
+                                      self.year)
+
+class Course(models.Model):
+    code = models.CharField(max_length=10)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    
+    def __str__(self):
+        return "{}: {}".format(self.code, self.name)
+
+class Application(models.Model):
+    applicant = models.ForeignKey(TeachingAssistant, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    rank = models.IntegerField(blank=False)
+    experience = models.IntegerField(blank=False)
+    
