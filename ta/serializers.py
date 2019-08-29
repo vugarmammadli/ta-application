503ea6cd9ceedb47108497d29963aa4fadfdcddb
vugarmammadli <v-m13@windowslive.com>
2019-08-29 08:10:49 -0400

    Created serializer for Course model

--- ta/serializers.py
+++ ta/serializers.py
@@ -2,11 +2,21 @@ from rest_framework import serializers
 from .models import TeachingAssistant, Course, Application
 
 class TeachingAssistantSerializer(serializers.ModelSerializer):
-    '''Serializer to map the Model instance to JSON format.
+    '''Serializer to map the TA model instance to JSON format.
     '''
     
     class Meta:
-        '''Meta class to map serializer's fields with the model fields.
+        '''Meta class to map serializer fields with the model fields.
         '''
         model = TeachingAssistant
         fields = ('student_number', 'given_name', 'family_name', 'status', 'year')
+
+class CourseSerializer(serializers.ModelSerializer):
+    '''Serializer to map the Course model instance to JSON format.
+    '''
+    
+    class Meta:
+        '''Meta class to map serializer fields with the model fields.
+        '''
+        model = Course
+        fields = ('code', 'name', 'description')
