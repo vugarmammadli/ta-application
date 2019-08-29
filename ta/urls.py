2159c1f84d5d0ad9409bd4df537f174ffa1e216f
vugarmammadli <v-m13@windowslive.com>
2019-08-29 08:22:50 -0400

    implemented GET POST /courses/, GET /courses?course=code

--- ta/urls.py
+++ ta/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'ta'
 urlpatterns = [
     # /applicants/
-    path('applicants/', views.TACreateView.as_view(), name='create'),
-    path('applicant/<int:pk>/', views.TADetailsView.as_view(), name='details'),
+    path('applicants/', views.TACreateView.as_view(), name='ta_create'),
+    path('applicant/<int:pk>/', views.TADetailsView.as_view(), name='ta_details'),
+    # /courses/
+    path('courses/', views.CourseCreateView.as_view(), name='course_create'),
 ]
--- ta/views.py
+++ ta/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 from rest_framework import generics
-from .serializers import TeachingAssistantSerializer
-from .models import TeachingAssistant
+from .serializers import TeachingAssistantSerializer, CourseSerializer
+from .models import TeachingAssistant, Course
 
 class TACreateView(generics.ListCreateAPIView):
     '''Handles the GET and POST requests of the rest api for TAs.
@@ -41,3 +41,29 @@ class TADetailsView(generics.RetrieveDestroyAPIView):
     '''
     serializer_class = TeachingAssistantSerializer
     queryset = TeachingAssistant.objects.all()
+
+class CourseCreateView(generics.ListCreateAPIView):
+    '''Handles the GET and POST requests of the rest api for courses.
+    '''
+    serializer_class = CourseSerializer
+    
+    def get_queryset(self):
+        '''Return a list of all the courses. Optionally filters courses
+        according to the course code.
+        '''
+        
+        queryset = Course.objects.all()
+        
+        # /courses?course=code
+        code = self.request.query_params.get('course')
+        
+        # iexact used for filtering case insensitive
+        if code is not None:
+            queryset = queryset.filter(code__iexact=code)
+        
+        return queryset
+    
+    def perform_create(self, serializer):
+        '''Save the post data when creating a new TA.
+        '''
+        serializer.save()    
